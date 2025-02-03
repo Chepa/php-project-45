920d217c5cf2da7d7e98ba0ae76d671ac07298c5
Dmitrii <dmitrii.chepurko@gmail.com>
2025-02-03 11:07:06 +0600

    Шаг 5

--- src/Cli.php
+++ src/Cli.php
@@ -8,3 +8,13 @@ use function cli\prompt;
 line('Welcome to the Brain Games!');
 $name = prompt('May I have your name?');
 line("Hello, %s!", $name);
+line('Answer "yes" if the number is even, otherwise answer "no".');
+$number = rand(1, 100);
+$answer = prompt('Question: ' . $number);
+
+if (($number % 2 == 0 && $answer == 'yes') || ($number % 2 !== 0 && $answer == 'no')) {
+    line('Correct!');
+} else {
+    line("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    line("Let's try again, %s!", $name);
+}
