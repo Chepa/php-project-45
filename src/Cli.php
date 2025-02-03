f00eb7a43f4983a411656d9210b5faba82f1c63d
Dmitrii <dmitrii.chepurko@gmail.com>
2025-02-03 10:03:50 +0600

    Шаг 3

---
+++ src/Cli.php
@@ -0,0 +1,9 @@
+<?php
+include 'vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+line('Welcome to the Brain Games!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
