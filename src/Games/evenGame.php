10a6b6bb439885fc20186a7940f9456eac812f25
Dmitrii <dmitrii.chepurko@gmail.com>
2025-02-11 16:46:50 +0600

    bug fix

--- src/Games/evenGame.php
+++ src/Games/evenGame.php
@@ -6,13 +6,12 @@ use function cli\line;
 use function cli\prompt;
 use function Engine\greeting as greeting;
 
-function evenGame(): void
+function evenGame(): bool
 {
     $name = greeting();
     line('Answer "yes" if the number is even, otherwise answer "no".');
 
     $total = 3;
-    $countPositive = 0;
     for ($i = 0; $i < $total; $i++) {
         $number = rand(1, 100);
         $line =  'Question: ' . $number;
@@ -22,18 +21,19 @@ function evenGame(): void
 
         if ($results) {
             line('Correct!');
-            $countPositive++;
         } else {
             line("'yes' is wrong answer ;(. Correct answer was 'no'.");
             $line = sprintf("Let's try again, %s!", $name);
             line($line);
+
+            return false;
         }
     }
 
-    if ($countPositive == $total) {
-        $line = sprintf("Congratulations, %s!", $name);
-        line($line);
-    }
+    $line = sprintf("Congratulations, %s!", $name);
+    line($line);
+
+    return true;
 }
 
 function game($number, $answer): bool
